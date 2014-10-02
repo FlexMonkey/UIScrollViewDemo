f1b343c238e1924c58763c12252e06ac4e9113cc
Simon Gladman <simongladman@Simons-MacBook-Pro.local>
2014-10-02 20:56:16 +0100

    Changed curve layer to CALayer and draw curves immediately - i.e. not with set needs display. Very quick!

--- UIScrollViewDemo/RelationshipCurvesLayer.swift
+++ UIScrollViewDemo/RelationshipCurvesLayer.swift
@@ -9,51 +9,36 @@
 import Foundation
 import UIKit
 
-class RelationshipCurvesLayer: CALayer
+class RelationshipCurvesLayer: CAShapeLayer
 {
-    private var curvesDirty: Bool = false
-    var path = UIBezierPath()
+    var relationshipCurvesPath = UIBezierPath()
     
-    func redrawRelationshipCurves()
+    final func redrawRelationshipCurves()
     {
-        drawsAsynchronously = true
+        strokeColor = UIColor.yellowColor().CGColor
+        lineWidth = 3
+        fillColor = nil
         
-        curvesDirty = true
+        relationshipCurvesPath.removeAllPoints()
         
-        setNeedsDisplay()
-    }
-    
-    override func drawInContext(ctx: CGContext!)
-    {
-        println("draw curves drawInContext")
-        
-        if curvesDirty
+        for targetNode in NodesPM.nodes
         {
-            curvesDirty = false
-            
-            path.removeAllPoints()
-            
-            for targetNode in NodesPM.nodes
+            for inputNode in targetNode.inputNodes
             {
-                for inputNode in targetNode.inputNodes
-                {
-                    let targetPosition = CGPoint(x: targetNode.position.x + 75, y: targetNode.position.y)
-                    let inputPosition = CGPoint(x: inputNode.position.x + 75, y: inputNode.position.y + 150)
-                    
-                    let controlPointOne = CGPoint(x: targetNode.position.x + 75, y: targetNode.position.y - 50)
-                    let controlPointTwo = CGPoint(x: inputNode.position.x + 75, y: inputNode.position.y + 150 + 50)
-                    
-                    path.moveToPoint(targetPosition)
-                    
-                    path.addCurveToPoint(inputPosition, controlPoint1: controlPointOne, controlPoint2: controlPointTwo)
-                }
+                let targetPosition = CGPoint(x: targetNode.position.x + 75, y: targetNode.position.y)
+                let inputPosition = CGPoint(x: inputNode.position.x + 75, y: inputNode.position.y + 150)
+                
+                let controlPointOne = CGPoint(x: targetNode.position.x + 75, y: targetNode.position.y - 50)
+                let controlPointTwo = CGPoint(x: inputNode.position.x + 75, y: inputNode.position.y + 150 + 50)
+                
+                relationshipCurvesPath.moveToPoint(targetPosition)
+                
+                relationshipCurvesPath.addCurveToPoint(inputPosition, controlPoint1: controlPointOne, controlPoint2: controlPointTwo)
             }
-            
-            CGContextAddPath(ctx, path.CGPath)
-            CGContextSetStrokeColorWithColor(ctx, UIColor.blueColor().CGColor)
-            CGContextSetLineWidth(ctx, 3)
-            CGContextStrokePath(ctx)
         }
+        
+        self.path = relationshipCurvesPath.CGPath
     }
+    
     
 }
